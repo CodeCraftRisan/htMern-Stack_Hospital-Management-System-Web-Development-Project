b47a2aef2c8cb4dbfff4f0baf6d186f50c8f888e
CodeCraftRisan <ahmedrisan01712@gmail.com>
2024-09-25 19:43:24 +0600

    middlewears files created

--- Backend/models/userSchema.js
+++ Backend/models/userSchema.js
@@ -16,5 +16,14 @@ import {
 
 const router = express.Router();
 
+router.post("/patient/register", patientRegister);
+router.post("/login", login);
+router.post("/admin/addnew", isAdminAuthenticated, addNewAdmin);
+router.post("/doctor/addnew", isAdminAuthenticated, addNewDoctor);
+router.get("/doctors", getAllDoctors);
+router.get("/patient/me", isPatientAuthenticated, getUserDetails);
+router.get("/admin/me", isAdminAuthenticated, getUserDetails);
+router.get("/patient/logout", isPatientAuthenticated, logoutPatient);
+router.get("/admin/logout", isAdminAuthenticated, logoutAdmin);
 
 export default router;
