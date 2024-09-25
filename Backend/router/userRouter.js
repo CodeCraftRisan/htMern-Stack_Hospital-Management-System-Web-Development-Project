f78702b351c5fe9bfe57e569a125a3e6c8e29982
CodeCraftRisan <ahmedrisan01712@gmail.com>
2024-09-25 20:15:13 +0600

    backend work has completely done and connected to frontend

--- Backend/router/userRouter.js
+++ Backend/router/userRouter.js
@@ -1,4 +1,18 @@
 import express from "express";
+import {
+  addNewAdmin,
+  addNewDoctor,
+  getAllDoctors,
+  getUserDetails,
+  login,
+  logoutAdmin,
+  logoutPatient,
+  patientRegister,
+} from "../controller/userController.js";
+import {
+  isAdminAuthenticated,
+  isPatientAuthenticated,
+} from "../middlewares/auth.js";
 
 const router = express.Router();
 
