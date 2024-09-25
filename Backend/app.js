a458592d4394a191171f3648280ad1aec24d4868
CodeCraftRisan <ahmedrisan01712@gmail.com>
2024-09-25 19:00:04 +0600

    backend work starting

---
+++ Backend/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import { dbConnection } from "./database/dbConnection.js";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+
+const app = express();
+config({ path: "./config.env" });
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL_ONE, process.env.FRONTEND_URL_TWO],
+    method: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
