a53b65891d199c66ebbfcb3aaacd7276e9719851
CodeCraftRisan <ahmedrisan01712@gmail.com>
2024-09-25 19:10:44 +0600

    some files code completed

--- Backend/models/appointmentSchema.js
+++ Backend/models/appointmentSchema.js
@@ -8,14 +8,28 @@ const appointmentSchema = new mongoose.Schema({
     required: [true, "First Name Is Required!"],
     minLength: [3, "First Name Must Contain At Least 3 Characters!"],
   },
- 
+  lastName: {
+    type: String,
+    required: [true, "Last Name Is Required!"],
+    minLength: [3, "Last Name Must Contain At Least 3 Characters!"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email Is Required!"],
+    validate: [validator.isEmail, "Provide A Valid Email!"],
+  },
   phone: {
     type: String,
     required: [true, "Phone Is Required!"],
     minLength: [11, "Phone Number Must Contain Exact 11 Digits!"],
     maxLength: [11, "Phone Number Must Contain Exact 11 Digits!"],
   },
-  
+  nic: {
+    type: String,
+    required: [true, "NIC Is Required!"],
+    minLength: [13, "NIC Must Contain Only 13 Digits!"],
+    maxLength: [13, "NIC Must Contain Only 13 Digits!"],
+  },
   dob: {
     type: Date,
     required: [true, "DOB Is Required!"],
@@ -42,4 +56,29 @@ const appointmentSchema = new mongoose.Schema({
       type: String,
       required: [true, "Doctor Name Is Required!"],
     },
- 
+  },
+  hasVisited: {
+    type: Boolean,
+    default: false,
+  },
+  address: {
+    type: String,
+    required: [true, "Address Is Required!"],
+  },
+  doctorId: {
+    type: mongoose.Schema.ObjectId,
+    required: [true, "Doctor Id Is Invalid!"],
+  },
+  patientId: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: [true, "Patient Id Is Required!"],
+  },
+  status: {
+    type: String,
+    enum: ["Pending", "Accepted", "Rejected"],
+    default: "Pending",
+  },
+});
+
+export const Appointment = mongoose.model("Appointment", appointmentSchema);
