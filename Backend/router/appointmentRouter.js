08519e29b196abb8deeb8b90420e44e9db3371c2
CodeCraftRisan <ahmedrisan01712@gmail.com>
2024-09-25 19:17:41 +0600

    backend work is running

--- Backend/router/appointmentRouter.js
+++ Backend/router/appointmentRouter.js
@@ -5,7 +5,12 @@ import {
   postAppointment,
   updateAppointmentStatus,
 } from "../controller/appointmentController.js";
+import {
+  isAdminAuthenticated,
+  isPatientAuthenticated,
+} from "../middlewares/auth.js";
 
+const router = express.Router();
 
 router.post("/post", isPatientAuthenticated, postAppointment);
 router.get("/getall", isAdminAuthenticated, getAllAppointments);
