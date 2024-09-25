cb42fe06ec4b7281f18a46cd3702ec6018340ed5
CodeCraftRisan <ahmedrisan01712@gmail.com>
2024-09-25 19:09:41 +0600

    some backend work has done

---
+++ Backend/controller/appointmentController.js
@@ -0,0 +1,70 @@
+import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
+import ErrorHandler from "../middlewares/error.js";
+import { Appointment } from "../models/appointmentSchema.js";
+import { User } from "../models/userSchema.js";
+
+export const postAppointment = catchAsyncErrors(async (req, res, next) => {
+  const {
+    firstName,
+    lastName,
+    email,
+    phone,
+    nic,
+    dob,
+    gender,
+    appointment_date,
+    department,
+    doctor_firstName,
+    doctor_lastName,
+    hasVisited,
+  
+      new ErrorHandler(
+        "Doctors Conflict! Please Contact Through Email Or Phone!",
+        400
+      )
+    );
+  }
+  const doctorId = isConflict[0]._id;
+  const patientId = req.user._id;
+  const appointment = await Appointment.create({
+    firstName,
+    lastName,
+    email,
+    phone,
+    nic,
+    dob,
+    gender,
+    appointment_date,
+    department,
+    doctor: {
+      firstName: doctor_firstName,
+      lastName: doctor_lastName,
+    },
+    hasVisited,
+    address,
+    doctorId,
+    patientId,
+  });
+  res.status(200).json({
+    success: true,
+    appointment,
+    message: "Appointment Send!",
+  });
+});
+
+
+    });
+  }
+);
+export const deleteAppointment = catchAsyncErrors(async (req, res, next) => {
+  const { id } = req.params;
+  const appointment = await Appointment.findById(id);
+  if (!appointment) {
+    return next(new ErrorHandler("Appointment Not Found!", 404));
+  }
+  await appointment.deleteOne();
+  res.status(200).json({
+    success: true,
+    message: "Appointment Deleted!",
+  });
+});
---
+++ Backend/models/appointmentSchema.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+import { Mongoose } from "mongoose";
+import validator from "validator";
+
+const appointmentSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, "First Name Is Required!"],
+    minLength: [3, "First Name Must Contain At Least 3 Characters!"],
+  },
+ 
+  phone: {
+    type: String,
+    required: [true, "Phone Is Required!"],
+    minLength: [11, "Phone Number Must Contain Exact 11 Digits!"],
+    maxLength: [11, "Phone Number Must Contain Exact 11 Digits!"],
+  },
+  
+  dob: {
+    type: Date,
+    required: [true, "DOB Is Required!"],
+  },
+  gender: {
+    type: String,
+    required: [true, "Gender Is Required!"],
+    enum: ["Male", "Female"],
+  },
+  appointment_date: {
+    type: String,
+    required: [true, "Appointment Date Is Required!"],
+  },
+  department: {
+    type: String,
+    required: [true, "Department Name Is Required!"],
+  },
+  doctor: {
+    firstName: {
+      type: String,
+      required: [true, "Doctor Name Is Required!"],
+    },
+    lastName: {
+      type: String,
+      required: [true, "Doctor Name Is Required!"],
+    },
+ 
